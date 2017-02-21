a2e8e913480a8d63361fcf7ae5c9cd91a95dd1f8
andrmos <mossige.a@gmail.com>
2017-02-21 18:09:31 +0100

    Add arraylist containing todos

--- app/src/main/java/com/example/fuii/todoapp/MainActivity.java
+++ app/src/main/java/com/example/fuii/todoapp/MainActivity.java
@@ -4,16 +4,26 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ListView;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
 
     private ListView todoListView;
+    private ArrayList<String> todos;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        todos = new ArrayList<>();
+        todos.add("Kjøpe mat");
+        todos.add("Kjøpe pils");
+        todos.add("Lære Android utvikling");
+
         todoListView = (ListView) findViewById(R.id.todoList);
+
+
 
     }
 }
