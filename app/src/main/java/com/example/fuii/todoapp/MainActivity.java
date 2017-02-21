9aeada89f6de7373e9b1328ff7970804d56ca107
andrmos <mossige.a@gmail.com>
2017-02-21 18:36:56 +0100

    Add entered todo to list

--- app/src/main/java/com/example/fuii/todoapp/MainActivity.java
+++ app/src/main/java/com/example/fuii/todoapp/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.fuii.todoapp;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
@@ -13,6 +14,7 @@ public class MainActivity extends AppCompatActivity {
 
     private ListView todoListView;
     private ArrayList<String> todos;
+    private ArrayAdapter<String> todoListAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,7 +27,7 @@ public class MainActivity extends AppCompatActivity {
         todos.add("Lære Android utvikling");
 
         todoListView = (ListView) findViewById(R.id.todoList);
-        ArrayAdapter<String> todoListAdapter = new ArrayAdapter<>(this, R.layout.todo_element, todos);
+        todoListAdapter = new ArrayAdapter<>(this, R.layout.todo_element, todos);
         todoListView.setAdapter(todoListAdapter);
 
     }
@@ -33,5 +35,15 @@ public class MainActivity extends AppCompatActivity {
     public void startNewTodoActivity(View view) {
         Intent intent = new Intent(this, NewTodoActivity.class);
         startActivityForResult(intent, 1);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == 1) {
+            Bundle bundle = data.getExtras();
+            String newTodo = bundle.getString("todoText");
+            todos.add(newTodo);
+            todoListAdapter.notifyDataSetChanged();
+        }
     }
 }
