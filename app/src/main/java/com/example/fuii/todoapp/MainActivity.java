2b8279373a6bfd2baecd73906ac22e1a3465b137
andrmos <mossige.a@gmail.com>
2017-02-21 18:07:52 +0100

    Add ListView to MainActivity

--- app/src/main/java/com/example/fuii/todoapp/MainActivity.java
+++ app/src/main/java/com/example/fuii/todoapp/MainActivity.java
@@ -2,12 +2,18 @@ package com.example.fuii.todoapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ListView todoListView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        todoListView = (ListView) findViewById(R.id.todoList);
+
     }
 }
