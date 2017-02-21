d89b2cc293f06ca547e56734ef412e059ca5d2b5
andrmos <mossige.a@gmail.com>
2017-02-21 18:23:05 +0100

    Generate 'new todo' activity

---
+++ app/src/main/java/com/example/fuii/todoapp/NewTodoActivity.java
@@ -0,0 +1,13 @@
+package com.example.fuii.todoapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NewTodoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_todo);
+    }
+}
