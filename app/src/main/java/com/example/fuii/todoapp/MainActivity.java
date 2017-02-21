00e130e3e230a3f6a85fa078769e6fe9022f49aa
andrmos <mossige.a@gmail.com>
2017-02-21 18:49:59 +0100

    Change action bar titles

--- app/src/main/java/com/example/fuii/todoapp/MainActivity.java
+++ app/src/main/java/com/example/fuii/todoapp/MainActivity.java
@@ -20,6 +20,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setTitle(R.string.app_name);
 
         todos = new ArrayList<>();
         todos.add("Kjøpe mat");
--- app/src/main/java/com/example/fuii/todoapp/NewTodoActivity.java
+++ app/src/main/java/com/example/fuii/todoapp/NewTodoActivity.java
@@ -16,6 +16,7 @@ public class NewTodoActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_todo);
+        setTitle(R.string.create_new_todo);
         inputField = (EditText) findViewById(R.id.newTodoInput);
     }
 
