b5432c64e689c1d2d3832b7e286af62fb2e0af21
andrmos <mossige.a@gmail.com>
2017-02-21 18:28:50 +0100

    Add confirm button to NewTodoActivity

--- app/src/main/java/com/example/fuii/todoapp/NewTodoActivity.java
+++ app/src/main/java/com/example/fuii/todoapp/NewTodoActivity.java
@@ -2,6 +2,7 @@ package com.example.fuii.todoapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class NewTodoActivity extends AppCompatActivity {
 
@@ -9,5 +10,8 @@ public class NewTodoActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_todo);
+    }
+
+    public void createNewTodo(View view) {
     }
 }
